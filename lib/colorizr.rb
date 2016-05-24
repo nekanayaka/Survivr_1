5be3f5bf233f21ad4fa29d85eafb561314b97ede
Nimna <nimna@nimna.me>
2016-05-24 21:16:56 +0000

    Colorizr gem is built and String class is created in the colorizr source.

---
+++ lib/colorizr.rb
@@ -0,0 +1,43 @@
+require 'colorizr'
+
+class String
+  def red
+    #Your code goes here
+  end
+
+  def green
+    #Your code goes here
+  end
+
+  def yellow
+    #Your code goes here
+  end
+
+  def blue
+    #Your code goes here
+  end
+
+  def pink
+    #Your code goes here
+  end
+
+  def light_blue
+    #Your code goes here
+  end
+
+  def white
+    #Your code goes here
+  end
+
+  def light_grey
+    #Your code goes here
+  end
+
+  def black
+    #Your code goes here
+  end
+  
+  def self.create_colors
+    #code to generate all color methods goes here
+  end
+end
