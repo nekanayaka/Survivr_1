017d8b2488d27f3200c049b2cfb905781a056125
Nimna <nimna@nimna.me>
2016-05-29 03:12:42 +0000

    Removed the method for each color and uses the create color method to generate the methods when needed.

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -3,46 +3,20 @@ require 'colorizr'
 class String
   attr_accessor :string
 
-  @@colors = String.methods
-
-  def red
-    @string = "\e[31m#{@string}\e[0m"
-  end
-
-  def green
-    @string = "\e[32m#{@string}\e[0m"
-  end
-
-  def yellow
-    @string = "\e[33m#{@string}\e[0m"
-  end
-
-  def blue
-    @string = "\e[34m#{@string}\e[0m"
-  end
-
-  def pink
-    @string = "\e[35m#{@string}\e[0m"
-  end
-
-  def light_blue
-    @string = "\e[94m#{@string}\e[0m"
-  end
-
-  def white
-    @string = "\e[97m#{@string}\e[0m"
-  end
-
-  def light_grey
-    @string = "\e[37m#{@string}\e[0m"
-  end
-
-  def black
-    @string = "\e[30m#{@string}\e[0m"
-  end
+  @@colors = {red: 31, green: 32, yellow: 33, blue: 34, pink: 35, light_blue: 94, white: 97, light_grey: 37, black: 30}
 
   def self.create_colors
-    #code to generate all color methods goes here
+    for color, value in @@colors
+      self.send(:define_method, color) do
+        puts "\e[#{value}m" + self + "\e[0m"
+      end
+    end
+  end
+  
+  def self.sample_colors
+    for color, value in @@colors
+      puts "This is #{color}".send(color)
+    end
   end
 
   def self.colors
@@ -50,4 +24,10 @@ class String
   end
 end
 
+# TESTING
+# -------
+
+String.create_colors
+# "Nimna".red
 puts String.colors
+puts String.sample_colors
